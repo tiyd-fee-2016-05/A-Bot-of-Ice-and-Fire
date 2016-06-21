3d9a886cb4086ede3a2de5e253aecdebe2eff867
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-06-20 20:39:12 -0400

    Tyrion lists commands -- @help

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,26 @@
 console.log("Ready to go!");
 
+
+//WHEN USER SUBMITS FORM//
+
 $('.talktoBot').on('submit', function (e) {
-  e.preventDefault()
+  e.preventDefault()//KEEPS FROM REFRESHING
 
-  var commandtext = $('input[class="command"]').val();
+  var commandtext = $('input[class="command"]').val();//ASSIGNING VARIABLE TO WHATEVER INPUT IS
     console.log("The user submitted" + commandtext)
-  // console.log(commandtext);
-  var commands = [];
-      commands.push(commandtext)
 
-console.log("You typed " + commands[0])
+  var commands = []; //OPEN ARRAY
+      commands.push(commandtext)//PUT THE INPUT CONTENT INTO THIS ARRAY
+
+console.log("You typed " + commands[0])//TELLS FIRST <LI>
 
 if (commandtext == "@help") {
   console.log("Tyrion can help.")
 
-
   $(".tyrionEntry").append('<li class="response">' + "How can I help?"+ '</li>')
-
-
-
+  $(".tyrionEntry").append('<li class="response">' + "@background (change the window's color)"+ '</li>')
+  $(".tyrionEntry").append('<li class="response">' + "@gif (search the Seven Kingdoms for gifs)"+ '</li>')
+  $(".tyrionEntry").append('<li class="response">' + "@a-song-of-ice-and-fire (hear what Westeros is listening to) "+ '</li>')
 
 
 };
