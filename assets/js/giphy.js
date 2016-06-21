6571515660912c096c8f2f96657b288df0eb8dcd
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-06-21 12:36:55 -0400

    Got call to Giphy working! Yay

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -1,22 +1,31 @@
 //Giphy Ajax Call
+$('.talktoBot').on('submit', function (e) {
+  e.preventDefault()//KEEPS FROM REFRESHING
 
-else if (commandtext == "@gif") {
-  console.log("Tyrion will find gifs.")
+var commandtext = $('input[class="command"]').val();//ASSIGNING VARIABLE TO WHATEVER USER INPUTS
+  console.log("The user submitted" + commandtext)
 
-// $ajax({
-//   url: URL WITH SEARCH CRITERIA HERE
-//   dataType:"JSON",
-//   method: "GET",
-//   })
-//   done.(function(data){
-//     console.log("Ajax call to the Giphy API");
-//     console.log(data);
-//
-//     for (var i = 0; i < data.length; i++){
-//       var gifUrl = (data[i].url)
-//
-//     $(".tyrionEntry").append('<li class="response">' + gifUrl + '</li>')
-//     }
-//   }
+  var keyword = commandtext.split(" ");
+  console.log(commandtext);
 
-};
+  console.log("Tyrion will find gifs about " + keyword);
+
+if (commandtext == "@gif") {
+  // console.log("Tyrion will find gifs about " + keyword);
+
+
+$.ajax({
+  url: "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC",
+  dataType:"JSON",
+  method: "GET",
+}).done(function(JSON) {
+    console.log("Ajax call to the Giphy API");
+    console.log(JSON);
+
+    // for (var i = 0; i < JSON.length; i++){
+      var gifUrl = (JSON.data[0].images.downsized_large.url)
+
+    $(".tyrionEntry").append('<li class="response">' + "<img src='" + gifUrl + "'/>" + '</li>');
+  });
+}
+});
