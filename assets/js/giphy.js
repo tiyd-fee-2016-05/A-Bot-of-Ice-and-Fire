0a151956534d3c7eda7f4eca0359f7843aa1aa11
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-06-22 07:17:18 -0400

    Progress update

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -12,12 +12,15 @@ var commandtext = $('input[class="command"]').val();//ASSIGNING VARIABLE TO WHAT
 
   // console.log("Tyrion will find gifs about " + gifKeyword);
 
+//Had to .search for "@gif" in commandtext input.
 var gifCheck = commandtext.search("@gif");
   console.log(gifCheck);
   console.log("Tyrion will find gifs about " + gifKeyword);
-
+//Test .search results by seeing Boolean presence of "@gif"-- AKA, if it were to say "@bif" instead, it will not trigger function with Ajax call. Additional fail-safe for other commands.
 if (gifCheck > -1){
 console.log("@gif is there!")
+
+//Ajax call for Giphy
 
 $.ajax({
   url: "http://api.giphy.com/v1/gifs/search?q=" + gifKeyword + "&api_key=dc6zaTOxFJmzC",
@@ -25,12 +28,11 @@ $.ajax({
   method: "GET",
 }).done(function(data) {
     console.log("Ajax call to the Giphy API");
-    console.log(data.data[0].images.downsized.url);
+    console.log(data.data[0].images.downsized.url); //Telling Giphy API what to console log. Worked slowly piece by to figure out what Giphy's API looked for, level by level.
 
-    // for (var i = 0; i < JSON.length; i++){
-      var gifUrl = (data.data[0].images.downsized.url)
+      var gifUrl = (data.data[0].images.downsized.url)//Telling Giphy API specifically what we are looking for
 
-    $(".tyrionEntry").append('<li class="response">' + "<img src='" + gifUrl + "'/>" + '</li>');
+    $(".tyrionEntry").append('<li class="response">' + "<img src='" + gifUrl + "'/>" + '</li>'); //Appending result of search to chat window
   });
 };
 });
